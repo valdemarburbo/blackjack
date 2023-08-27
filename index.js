7dd34c1aa811e73add7f45fd2d453af4bc8d100f
Valdemar Burbo <valdemar.burbo@gmail.com>
2023-08-27 21:48:14 +0100

    Add start button

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-let firstCard = 8;
+let firstCard = 10;
 let secondCard = 11;
 let sum = firstCard + secondCard;
 
@@ -7,15 +7,18 @@ let isAlive = true;
 
 let message = "";
 
-if (sum <= 20) {
-    message = "Do you want to draw a new card?"
-} else if (sum === 21) {
-    message = "Congratulations, you have a blackjack!"
-    hasBlackJack = true;
-
-} else {
-    message = "You lose!"
-    isAlive = false;
+function startGame() {
+    if (sum <= 20) {
+        message = "Do you want to draw a new card?"
+    } else if (sum === 21) {
+        message = "Congratulations, you have a blackjack!"
+        hasBlackJack = true;
+    } else {
+        message = "You lose!"
+        isAlive = false;
+    }
+    
+    alert(message)
 }
 
 console.log(message)
